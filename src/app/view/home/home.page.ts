1d1e8c3901f39b6bccc3892275e36471aa575691
Misael Alexandrino <151184035+MisaelAlexandrino264@users.noreply.github.com>
2024-07-05 09:51:14 -0300

    Add files via upload

---
+++ src/app/view/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Contato from 'src/app/model/entities/Contato';
+import { ContatoService } from 'src/app/model/services/contato.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  contatos : Contato[] = [];
+  
+
+  constructor(private router: Router, private contatoService: ContatoService) {
+    this.contatos = contatoService.obterTodos();
+    
+    
+    
+  }
+
+  irParaCadastrar(){
+    this.router.navigate(['/cadastrar']);
+  }
+
+}
