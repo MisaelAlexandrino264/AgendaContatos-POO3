e1b8a1166b6afd06223ea52905d3ec29aa7be701
Misael Alexandrino <151184035+MisaelAlexandrino264@users.noreply.github.com>
2024-07-19 09:25:44 -0300

    Add files via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyASXvW3ggO6bpZh_kwg-z0qMNY3n_6y32I",
+    authDomain: "appcontatos-67720.firebaseapp.com",
+    projectId: "appcontatos-67720",
+    storageBucket: "appcontatos-67720.appspot.com",
+    messagingSenderId: "505800581587",
+    appId: "1:505800581587:web:f72244a94762c271024f26"
+  }
 };
